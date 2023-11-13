75308df9e4746bdc752b0b7049525c58af69bbef
trolca <szgiemza@gmail.com>
2023-11-13 22:36:05 +0100

    Initial commit

---
+++ src/main/java/me/tololo11/voteplugin/VotePlugin.java
@@ -0,0 +1,17 @@
+package me.tololo11.voteplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VotePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
